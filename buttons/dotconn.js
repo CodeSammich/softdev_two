7036589967202f31c201c3649815f1ed4993f3ce
Samuel Zhang <samuelzhang3@gmail.com>
2016-02-11 14:05:52 -0500

    gets coors

--- buttons/dotconn.js
+++ buttons/dotconn.js
@@ -1,13 +1,44 @@
-var c = document.getContextById( "playground" );
+var c = document.getElementById( "playground" );
 var ctx = c.getContext( "2d" );
 
-var btn = document.getElementById( "clear" );
+ctx.fillStyle = "#66cccc";
+ctx.fillRect( 0, 0, 500, 500 );
 
-btn.addEventListener( "click", circle );
+c.addEventListener( "click", circle );
 
-function circle(e) {
+function circle(event) {
+    console.log("circle!");
+    var mouseX = event.offsetX;
+    var mouseY = event.offsetY;
+
+    console.log(mouseX);
+    
+    ctx.fillStyle = "#00868B";
     ctx.beginPath();
-    ctx.arc( e.clientX, e.clientY, 25, 0, 2*Math.Pi );
-    ctx.stroke();
+    ctx.arc( mouseX, mouseY, 25, 0, 2*Math.Pi );
+    ctx.fill();
     ctx.closePath();
 }
+
+/*
+function getMousePos( canvas, evt ) {
+    var rect = ctx.getBoundingClientRect();
+    return {
+	x: evt.clientX - rect.left,
+	y: evt.clientY - rect.top
+    };
+}
+*/
+
+/*
+function clear() {
+
+    
+}
+
+// Clear button 
+var btn = document.getElementById( "clear" );
+btn.addEventListener( "click", clear );
+*/
+
+
