7fdb012ebc559c53582459b8b49569961c2a3fda
Samuel Zhang <samuelzhang3@gmail.com>
2016-02-11 16:58:13 -0500

    circles, need clear

--- buttons/dotconn.js
+++ buttons/dotconn.js
@@ -1,47 +1,51 @@
-var c = document.getElementById( "playground" );
-var ctx = c.getContext( "2d" );
+var c = document.getElementById( "playground" ); //canvas
+var ctx = c.getContext( "2d" ); //context
 
 ctx.fillStyle = "#66cccc";
 ctx.fillRect( 0, 0, 500, 500 );
 
+//Circle generator
+var numberOfCircles = 0;
 c.addEventListener( "click", circle );
+
+// Clear button 
+var btn = document.getElementById( "clear" );
+btn.addEventListener( "click", clear );
+
+var prev_mouseX, prev_mouseY;
 
 function circle(event) {
     console.log("circle!");
     var mouseX = event.offsetX;
     var mouseY = event.offsetY;
-
     console.log(mouseX);
     console.log(mouseY);
     
     ctx.fillStyle = "#00868B";
-    
+    ctx.strokeStyle = "#00868B";
     ctx.beginPath();
-    ctx.fillRect( mouseX, mouseY, 25, 25 );
-//    ctx.arc( mouseX, mouseY, 25, 0, 2*Math.Pi, false );
-    ctx.stroke();
-    ctx.closePath();
-}
 
-/*
-function getMousePos( canvas, evt ) {
-    var rect = ctx.getBoundingClientRect();
-    return {
-	x: evt.clientX - rect.left,
-	y: evt.clientY - rect.top
-    };
-}
-*/
-
-/*
-function clear() {
-
+    if(numberOfCircles > 0) {
+	ctx.moveTo( prev_mouseX, prev_mouseY );
+	ctx.lineTo( mouseX, mouseY );
+    }
+    prev_mouseX = mouseX;
+    prev_mouseY = mouseY;
     
+    ctx.arc( mouseX, mouseY, 50, 0, 2*Math.PI );
+    ctx.fill();
+    
+    ctx.closePath();
+    numberOfCircles++;
 }
 
-// Clear button 
-var btn = document.getElementById( "clear" );
-btn.addEventListener( "click", clear );
-*/
+function clear(event) {
+    console.log("clear");
+    
+    ctx.fillStyle = "#66cccc";
+    ctx.fillRect( 0, 0, 500, 500 );
+}
+
+
 
 
