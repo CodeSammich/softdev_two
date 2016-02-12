0260f1d3765646d486e36a0e00e293ee28fe1c3c
Samuel Zhang <samuelzhang3@gmail.com>
2016-02-12 10:12:29 -0500

    efficiency and reorganization! good sample code

--- buttons/dotconn.js
+++ buttons/dotconn.js
@@ -1,46 +1,33 @@
 var c = document.getElementById( "playground" ); //canvas
 var ctx = c.getContext( "2d" ); //context
 
+//canvas
 ctx.fillStyle = "#66cccc";
 ctx.fillRect( 0, 0, 500, 500 );
 
 //Circle generator
-var numberOfCircles = 0;
 c.addEventListener( "click", circle );
 
 // Clear button 
 var btn = document.getElementById( "clear" );
 btn.addEventListener( "click", clear );
 
-var prev_mouseX, prev_mouseY;
+ctx.fillStyle = "#00868B";
+ctx.strokeStyle = "#00868B";
 
 function circle(event) {
-   // console.log("circle!");
-    var mouseX = event.offsetX;
-    var mouseY = event.offsetY;
-    //console.log(mouseX);
-   // console.log(mouseY);
-    
-    ctx.fillStyle = "#00868B";
-    ctx.strokeStyle = "#00868B";
+    //draw line
+    ctx.lineTo( event.offsetX, event.offsetY );
+    ctx.stroke();
+
+    //draw circle
     ctx.beginPath();
-
-    if(numberOfCircles > 0) {
-	ctx.moveTo( prev_mouseX, prev_mouseY );
-	ctx.lineTo( mouseX, mouseY );
-	ctx.stroke();
-	ctx.closePath();
-
-	ctx.beginPath();
-    }
-    prev_mouseX = mouseX;
-    prev_mouseY = mouseY;
-    
-    ctx.arc( mouseX, mouseY, 25, 0, 2*Math.PI );
+    ctx.arc( event.offsetX, event.offsetY, 25, 0, 2*Math.PI );
     ctx.fill();
     
-    ctx.closePath();
-    numberOfCircles++;
+    //prepare to draw next line
+    ctx.beginPath();
+    ctx.moveTo( event.offsetX, event.offsetY );
 }
 
 function clear(event) {
